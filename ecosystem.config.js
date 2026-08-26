8aba88880eac26651efb8a7c3546b44b4749ca3e
tengon <jtengon@gmail.com>
2026-08-26 12:33:35 +0700

    chore: configure log files and timestamp formatting for PM2 ecosystem processes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,6 +9,10 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      error_file: './logs/sipupuk-web-error.log',
+      out_file: './logs/sipupuk-web-out.log',
       env: {
         NODE_ENV: 'production',
         PORT: 3000
@@ -22,6 +26,10 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
+      time: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      error_file: './logs/sipupuk-scraper-error.log',
+      out_file: './logs/sipupuk-scraper-out.log',
       env: {
         NODE_ENV: 'production'
       }
